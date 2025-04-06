2e5db710be72e311433f017f6d2dbc04b80a69ba
kounou25 <kounougilbert288@gmail.com>
2025-04-06 01:24:03 +0100

    ajout de la verification CORS

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
 const PORT = 3000;
+
+// Middleware pour gérer les CORS
+app.use(cors({
+  origin: 'http://localhost:8080', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // Middleware pour comprendre le JSON
 app.use(express.json());
